312b7a5837e7783ad6e4c3d47a43043bcdfe5771
Maicol Santos <maicolcostaa8@gmail.com>
2022-03-03 13:56:55 +0000

    Create amination to show footer info

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,27 @@
+import { useEffect, useState } from 'react'
+
 import './styles.css'
 
-export const Footer = () => (
-  <footer className="footer">
-    <div className="footer-container">
-      <h1>Maicol Santos</h1>
-      <a href="mailto:maicolcostaa8@gmail.com">maicolcostaa8@gmail.com</a>
-      <a href="https://github.com/maicolsantos" target="_blank">github</a>
-      <a href="https://www.linkedin.com/in/maicolsantos" target="_blank">linkedin</a>
-    </div>
-  </footer>
-)
+const TIMEOUT = 500 // 0.5s
+
+
+export const Footer = () => {
+  const [ showInfo, setShowInfo ] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowInfo(true)
+    }, TIMEOUT)
+  }, [])
+
+  return (
+    <footer className="footer">
+      <div className={`footer-container ${showInfo ? 'active' : ''}`}>
+        <h1>Maicol Santos</h1>
+        <a href="mailto:maicolcostaa8@gmail.com">maicolcostaa8@gmail.com</a>
+        <a href="https://github.com/maicolsantos" target="_blank">github</a>
+        <a href="https://www.linkedin.com/in/maicolsantos" target="_blank">linkedin</a>
+      </div>
+    </footer>
+  )
+}
